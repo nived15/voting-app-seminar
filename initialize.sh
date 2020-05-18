1f1175717d856270b437e8879bfda730ef5b45f3
Nived15 <nivedvelayudhan1508@gmail.com>
2020-05-18 09:37:32 +0530

    anisble added

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -e "INSTALLING CORE PACKAGES FOR DEMO\n"
-yum install git bash-completion vim tree yum-utils -y
+yum install git epel-release bash-completion vim tree yum-utils -y
+yum install ansible -y
 
 echo -e "CONFIGURING GIT FOR USER NIVEDV"
 echo -e 'Username is set!\n'
@@ -11,8 +12,4 @@ echo -e "Push method set to simple\n"
 git config --global push.method simple
 echo -e 'GIT CONFIG COMPLETE!!!'
 
-echo -e "CLONING YOUR SEMINAR DEMO PROJECT"
-git clone https://github.com/nived15/voting-app-seminar.git
-
-cd voting-app-seminar
 
